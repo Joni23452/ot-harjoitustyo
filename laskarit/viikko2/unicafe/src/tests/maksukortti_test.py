dec7524a3d653fd73f6e4772c78827788b356f02
Joni <joni.kaup@outlook.com>
2025-03-12 20:08:33 +0200

    Unicafe maksukortti tests done

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(50)
+        
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.5)
+
+    def test_rahan_ottaminen_vähentää_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(50)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 9.5)
+
+    def test_liian_rahan_ottaminen_ei_vähennä_saldoa(self):
+        self.maksukortti.ota_rahaa(1050)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_ota_rahaa_palauttaa_true_kun_otto_onnistuu(self):
+        result = self.maksukortti.ota_rahaa(50)
+
+        self.assertTrue(result)
+    
+    def test_ota_rahaa_palauttaa_false_kun_otto_epäonnistuu(self):
+        result = self.maksukortti.ota_rahaa(1050)
+
+        self.assertFalse(result)
