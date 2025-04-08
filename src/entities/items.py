c5303437b5cb26235b58d4b505647ad62622cf38
Joni <joni.kaup@outlook.com>
2025-04-08 17:25:14 +0300

    Items can be fetched by name

--- src/entities/items.py
+++ src/entities/items.py
@@ -12,11 +12,17 @@ class Item:
 class ItemsDatabase:
     def __init__(self):
         self.items = {}
+        self.item_name_to_key = {}
         self.next_key = 0
 
     def add_item(self, item: Item):
         self.items[self.next_key] = item
+        self.item_name_to_key[item.get_name()] = self.next_key
         self.next_key += 1
 
     def get_item_by_key(self, item_key):
         return self.items[item_key]
+
+    def get_item_by_name(self, item_name):
+        key = self.item_name_to_key[item_name]
+        return self.get_item_by_key(key)
