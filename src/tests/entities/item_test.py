6d88972f27081a769157771189df90bed512fdac
Joni <joni.kaup@outlook.com>
2025-04-12 19:26:36 +0300

    Renaming

--- src/tests/entities/item_test.py
+++ src/tests/entities/item_test.py
@@ -1,5 +1,5 @@
 import unittest
-from entities.items import Item, ItemsDatabase
+from entities.items import Item, ItemDatabase
 
 class TestItem(unittest.TestCase):
     def setUp(self):
@@ -11,18 +11,18 @@ class TestItem(unittest.TestCase):
     def test_get_value_returns_correct_value(self):
         self.assertEqual(self.item.get_value(), 1)
 
-class TestItemsDatabase(unittest.TestCase):
+class TestItemDatabase(unittest.TestCase):
     def setUp(self):
-        self.items = ItemsDatabase()
+        self.items = ItemDatabase()
         self.items.add_item(Item("kala", 1))
         self.items.add_item(Item("kivi", 10))
     
-    def test_first_item_added_is_found_at_key_0(self):
-        item = self.items.get_item_by_key(0)
+    def test_first_item_added_is_found_at_id_0(self):
+        item = self.items.get_item_by_id(0)
         self.assertEqual(item.get_name(), "kala")
     
     def test_name_to_key_returns_correct_key(self):
-        key = self.items.get_item_key_by_name("kala")
+        key = self.items.get_item_id_by_name("kala")
         self.assertEqual(key, 0)
 
 
