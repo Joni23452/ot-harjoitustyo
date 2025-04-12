549e587e7e05cac6a7be798f56124dc42bb22594
Joni <joni.kaup@outlook.com>
2025-04-12 20:31:55 +0300

    Item repository reads data from json file

--- src/repositories/item_repository.py
+++ src/repositories/item_repository.py
@@ -1,15 +1,26 @@
 from entities.items import Item, ItemDatabase
+import json
+import os
+
+DIRNAME = os.path.dirname(__file__)
+ITEMS_FILE_PATH = os.path.join(DIRNAME, "..", "data", "items.json")
 
 class ItemRepository:
     def __init__(self):
-        self.item_database = ItemDatabase()
-        self._load_items()
+        self._item_database = ItemDatabase()
+        self._load_items(ITEMS_FILE_PATH)
 
-    def _load_items(self):
-        self.item_database.add_item(Item("kala",1))
-        self.item_database.add_item(Item("kivi",10))
-        self.item_database.add_item(Item("talo",100))
-        self.item_database.add_item(Item("ötökkä",2))
+    def _load_items(self, file_path):
+        with open(file_path) as file:
+            items: list = json.load(file)
+        for item_data in items:
+            name: str = item_data["name"]
+            value: int = item_data["value"]
+            self._add_item_to_database(name, value)
+
+    def _add_item_to_database(self, name: str, value: int):
+        item = Item(name, value)
+        self._item_database.add_item(item)
 
     def get_item_name(self, item_id: int) -> str:
         item = self._get_item(item_id)
@@ -20,10 +31,10 @@ class ItemRepository:
         return item.get_value()
 
     def get_all_item_ids(self) -> list:
-        return self.item_database.get_ids()
+        return self._item_database.get_ids()
 
     def get_item_id_by_name(self, name) -> int:
-        return self.item_database.get_item_id_by_name(name)
+        return self._item_database.get_item_id_by_name(name)
         
     def _get_item(self, item_id: int) -> Item:
-        return self.item_database.get_item_by_id(item_id)
+        return self._item_database.get_item_by_id(item_id)
