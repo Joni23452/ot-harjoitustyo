26bec203b5665afeec6d0b741adb5d0e9a151b85
Joni <joni.kaup@outlook.com>
2025-04-12 22:46:30 +0300

    Inventory now saves to a file

--- src/services/item_service.py
+++ src/services/item_service.py
@@ -1,15 +1,15 @@
 from repositories.item_repository import ItemRepository
-from entities.inventory import Inventory
+from repositories.inventory_repository import InventoryRepository
 from services.random_service import RandomService
 
 class ItemService:
     def __init__(self):
-        self.inventory = Inventory()
+        self.inventory_repository = InventoryRepository()
         self.item_repository = ItemRepository()
         self.random_service = RandomService(self.item_repository)
 
     def get_inventory_content(self):
-        return self.inventory.get_content()
+        return self.inventory_repository.get_content()
 
     def get_item_name(self, item_id):
         item_name = self.item_repository.get_item_name(item_id)
@@ -17,12 +17,12 @@ class ItemService:
 
     def get_item(self):
         new_item = self.random_service.convert(0)
-        self.inventory.add_item(new_item)
+        self.inventory_repository.add_item(new_item)
 
     def convert_item(self, item_name):
         item_id = self.item_repository.get_item_id_by_name(item_name)
-        if self.inventory.has_item(item_id):
+        if self.inventory_repository.has_item(item_id):
             value = self.item_repository.get_item_value(item_id)
             new_item = self.random_service.convert(value)
-            self.inventory.remove_item(item_id)
-            self.inventory.add_item(new_item)
+            self.inventory_repository.remove_item(item_id)
+            self.inventory_repository.add_item(new_item)
